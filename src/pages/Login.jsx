05f2cc4ed20a0f928e9599740a5555074cec36e5
jcromerohdz <jcromerohdz@gmail.com>
2022-07-26 16:42:26 -0700

    Creating Login Form in the Login component and styling with tailwindcss

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,54 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <h1 className='text-sky-600 font-black text-6xl capitalize'>
+        Start session and manage your {' '} 
+        <span className='text-slate-700'>
+          projects
+        </span>
+      </h1>
+      <form 
+        className='my-10 bg-white showdow rounded-lg p-10'
+        action=""
+      >
+        <div className='my-5'>
+          <label 
+            className='uppercase text-gray-600 block text-xl font-bold'
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input 
+            id='email'
+            type="email" 
+            placeholder="Register Email"
+            className='w-full mt-3 p-3 border rounded-xl bg-gray-50'
+          />
+        </div>
+        <div className='my-5'>
+          <label 
+            className='uppercase text-gray-600 block text-xl font-bold'
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input 
+            id='password'
+            type="password" 
+            placeholder="Register Password"
+            className='w-full mt-3 p-3 border rounded-xl bg-gray-50'
+          />
+        </div>
+
+        <input 
+          type="submit" 
+          value="Start Session"
+          className='bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors'
+        />
+      </form>
+      
+    </>
   )
 }
 
